50dd0c62e264c166ac8616a38f27a22403f00cdb
kgwizdzi <116073883+kgwizdzi@users.noreply.github.com>
2023-07-26 15:59:02 +0200

    Add files via upload

--- run-producer-run2/prod.sh
+++ run-producer-run2/prod.sh
@@ -8,7 +8,6 @@ o2-analysis-trackselection --configuration json://$config_file -b |
 o2-analysis-timestamp  --configuration json://$config_file -b | 
 o2-analysis-multiplicity-table  --configuration json://$config_file -b |
 o2-analysis-centrality-table  --configuration json://$config_file -b |
-#o2-analysis-fdd-converter --configuration json://$config_file -b |
 o2-analysis-zdc-converter --configuration json://$config_file -b |
 o2-analysis-event-selection  --configuration json://$config_file -b |
 o2-analysis-trackextension  --configuration json://$config_file -b |
@@ -20,9 +19,9 @@ o2-analysis-pid-tof-beta --configuration json://$config_file --aod-memory-rate-l
 o2-analysis-pid-tof-full  --configuration json://$config_file --aod-memory-rate-limit 600000000 -b |
 o2-analysis-pid-tpc-full --configuration json://$config_file --aod-memory-rate-limit 600000000 -b |
 o2-analysis-lf-lambdakzerobuilder --configuration json://$config_file -b |
-#o2-analysis-hf-refit-pv-dummy --configuration json://$config_file --aod-memory-rate-limit 600000000 -b |
-#o2-analysis-track-to-collision-associator --configuration json://$config_file --aod-memory-rate-limit 600000000 -b |
-#o2-analysis-hf-candidate-creator-2prong --configuration json://$config_file --aod-memory-rate-limit 600000000 -b |
-#o2-analysis-hf-candidate-selector-d0 --configuration json://$config_file --aod-memory-rate-limit 600000000 -b |
+o2-analysis-hf-refit-pv-dummy --configuration json://$config_file --aod-memory-rate-limit 600000000 -b |
+o2-analysis-track-to-collision-associator --configuration json://$config_file --aod-memory-rate-limit 600000000 -b |
+o2-analysis-hf-candidate-creator-2prong --configuration json://$config_file --aod-memory-rate-limit 600000000 -b |
+o2-analysis-hf-candidate-selector-d0 --configuration json://$config_file --aod-memory-rate-limit 600000000 -b |
 o2-analysis-cf-femto-world-producer --configuration json://$config_file --aod-writer-json $writer_file -b #&>output.log &
 # o2-analysis-cf-femto-world-producer --configuration json://$config_file --aod-writer-resfile FemtoAO2D --aod-writer-keep AOD/FEMTOWORLDPARTS/0,AOD/FEMTOWORLDCOLS/0 -b &>output.log &
